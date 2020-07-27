48ced6253974b65799fa17b956d07e1a5a54bde4
Raul Montes <montesraul7@gmail.com>
2020-07-26 23:09:14 -0700

    Some Edits

--- Four Dark Devas of Destruction Bros/Assets/Script/Player.cs
+++ Four Dark Devas of Destruction Bros/Assets/Script/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xMovement = Input.GetAxis("Horizontal");
+
     }
 }

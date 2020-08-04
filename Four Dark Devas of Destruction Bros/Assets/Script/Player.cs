72652bee928059a21cebbc9e11a8c965f0fe2905
Raul Montes <montesraul7@gmail.com>
2020-08-04 13:53:38 -0700

    Jump Activated
    Jump Mechanic inserted for future jumping.

--- Four Dark Devas of Destruction Bros/Assets/Script/Player.cs
+++ Four Dark Devas of Destruction Bros/Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float jumpForce = 5f;
+    public int currentJumps;
     public int maxJumps = 1;
     public float speed = 3f;
     public float height = 0.65f;
@@ -15,6 +16,7 @@ public class Player : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        currentJumps = maxJumps;
     }
 
     // Update is called once per frame
@@ -28,17 +30,25 @@ public class Player : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (IsGrounded())
+            {
+                currentJumps = maxJumps;
+            }
+            if (currentJumps > 0)
+            {
+                Jump();
+            }
         }
     }
     void Jump()
     {
+        currentJumps--;
         rigidBody.AddForce(Vector2.up * jumpForce);
-
+        
     }
     bool IsGrounded()
     {
         RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, (height /2f) + 0.1f );
         return (hitinfo.collider != null);
-    }
+    } 
 }

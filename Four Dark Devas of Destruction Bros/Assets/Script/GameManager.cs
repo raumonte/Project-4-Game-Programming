4f2dbd5f0004bb98d809d6e1e064976516c961ba
Raul Montes <montesraul7@gmail.com>
2020-08-06 02:45:45 -0700

    New Scenes, Loading into Scenes, and Decorating Title Screen
    In this commit I made new edits to the game manager to have functions to load into scenes based on the build index. Then had a bit more function in the title screen scenes. Also added more scenes that will go into the build index to have the script go through with transferring into other scenes.

--- Four Dark Devas of Destruction Bros/Assets/Script/GameManager.cs
+++ Four Dark Devas of Destruction Bros/Assets/Script/GameManager.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public int currentSceneIndex = 0 ;
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;   
+    }
     private void Awake()
     {
         if (instance == null)
@@ -21,12 +27,46 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+   public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("scene finished loading");
+        currentSceneIndex = scene.buildIndex;
+    }
+    public void LoadNextScene()
+    {
+        LoadLevel(currentSceneIndex + 1);
+    }
+    public void LoadPreviousScene()
+    {
+        LoadLevel(currentSceneIndex - 1);
+    }
+    public void exitScene()
+    {
+        //Using this void I will be able to assign it to the button to exit the game once the button is pressed.
+        Debug.Log("It has been pressed!");
+        Application.Quit();
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            LoadNextScene();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            LoadPreviousScene();
+        }
     }
 }
